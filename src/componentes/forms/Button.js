7b4f0d1505df8eeac0a32a98fe6ad2dc1331e02e
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-03-01 10:32:28 -0300

    Arquivos calculadora

---
+++ src/componentes/forms/Button.js
@@ -0,0 +1,9 @@
+import styles from './Button.module.css'
+
+function Button(){
+    return(
+        <div className={`${styles.botao} ${styles.botao__sinais}`}>C</div>
+    )
+}
+
+export default Button
