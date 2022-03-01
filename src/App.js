15e6a0034ee34d5fa42253656e1e8032a5c07cc5
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-03-01 10:44:03 -0300

    Voltando arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from './componentes/layout/Container'
+import Visor from './componentes/layout/Visor'
+import Buttons from './componentes/layout/Buttons'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>App Danilo</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Visor/>
+      <Buttons/>
+    </Container>
   );
 }
 
